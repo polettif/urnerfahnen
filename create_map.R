134992a9e19a127ff717cbab17546aa7e439bd86
Flavio Poletti <flavio.poletti@hotmail.ch>
2025-08-03 15:46:58 +0200

    add location type

--- create_map.R
+++ create_map.R
@@ -4,10 +4,10 @@ library(mapview)
 coords.csv = read.csv("urnerfahnen-standorte.csv")
 coords = sf::st_as_sf(coords.csv, coords = c("lon", "lat"), crs = 4326)
 
-nrow(unique(round(coords.csv, 4))) == nrow(coords.csv)
+nrow(unique(round(coords.csv[,1:2], 4))) == nrow(coords.csv)
 
 map = leaflet() |>
-	addProviderTiles(providers$CartoDB.Positron) %>%
+	addProviderTiles(providers$CartoDB.Positron) |>
 	addCircleMarkers(data = coords,
 					fillColor = "#F8D755", color = "#121212", weight = 2,
 					opacity = 1, fillOpacity = 1)
