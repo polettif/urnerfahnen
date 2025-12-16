061b123d4c5e06c698c4c0f2499d4c1c8a964e2e
Flavio Poletti <flavio.poletti@hotmail.ch>
2025-12-16 12:09:22 +0100

    update locations

--- create_map.R
+++ create_map.R
@@ -3,6 +3,7 @@ library(mapview)
 
 coords.csv = read.csv("urnerfahnen-standorte.csv")
 coords = sf::st_as_sf(coords.csv, coords = c("lon", "lat"), crs = 4326)
+coords <- cbind(coords, coords.csv)
 
 nrow(unique(round(coords.csv[,1:2], 4))) == nrow(coords.csv)
 
@@ -10,7 +11,7 @@ map = leaflet() |>
 	addProviderTiles(providers$CartoDB.Positron) |>
 	addCircleMarkers(data = coords,
 					fillColor = "#F8D755", color = "#121212", weight = 2,
-					opacity = 1, fillOpacity = 1)
+					opacity = 1, fillOpacity = 1, popup = paste0(coords$lon, ", ", coords$lat))
 
 mapview::mapshot(map, url = "docs/index.html")
 unlink("docs/index_files", recursive = T)
